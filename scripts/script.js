1e2ee5184f069fc5d8829341f32459c144d2a571
EnErGGy129 <109727222+EnErGGy129@users.noreply.github.com>
2023-01-08 23:29:37 +0600

    080123

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
   $(".product-buy").click(function () {
     $(".modal-wrapper").toggleClass("hidden");
-  })
+  });
   $(".modal-close").click(function () {
     $(".modal-wrapper").toggleClass("hidden");
   });
 });
- $(document).ready(function () {
-  $('.header-burger').click(function (event) {
-  $('.header-burger,.nav,.header').toggleClass('active');
-  $('body').toggleClass('lock');
-   });
- });
+$(document).ready(function () {
+  $(".header-burger").click(function (event) {
+    $(".header-burger,.nav,.header").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+function preloader() {
+  $(document).ready(function () {
+    $("#overflow").css("overflow", "hidden");
+    setTimeout(function load() {
+      let loading = $(".preloader");
+      loading.css("display", "none");
+      $("#overflow").css("overflow", "hidden");
+    }, 4000);
+  });
+}
+preloader();
